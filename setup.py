9cbebe692441dab3786d354e6ca42812e315d66b
Micha Gorelick <mynameisfiber@gmail.com>
2015-04-09 16:29:58 -0400

    almost completely setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='lolssh',
+    version='0.2',
+    description='Server for lolssh so you can lolcommit on remote machines',
+    author='Micha Gorelick',
+    author_email='mynameisfiber@gmail.com',
+    url='http://github.com/mynameisfiber/lolssh/',
+    packages=['lolssh'],
+)
